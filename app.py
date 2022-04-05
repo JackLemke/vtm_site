388281865085b8071a4acd123e252727f4c38bc3
JackLemke <jackson-lemke@uiowa.edu>
2022-04-04 20:13:11 -0500

    updated all html files website working correctly

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', pageTitle = 'Flask Server Home Page')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle = 'About Page')
 
-@app.route('/jack')
-def jack():
-    return render_template('jack.html', pageTitle = 'About Jack')
+@app.route('/')
+def base():
+    return render_template('base.html', pageTitle = 'Home Page')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html', pageTitle = 'Estimate Page')
+
+@app.route('/index')
+def index():
+    return render_template('index.html', pageTitle = 'Index Page')     
 
 if __name__ == '__main__':
     app.run(debug=True)
