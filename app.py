a412dc003c4e1ee3318a2d75bdb5c365d92d569f
JackLemke <jackson-lemke@uiowa.edu>
2022-04-04 23:10:43 -0500

    updated estimate html and got image to work

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-
-   
+ 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -13,9 +12,22 @@ def about():
 def base():
     return render_template('base.html', pageTitle = 'Home Page')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['GET','POST'])
 def estimate():
-    return render_template('estimate.html', pageTitle = 'Estimate Page')
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['radius'])
+        height = float(form['height'])
+        pi = 3.14
+        top_tank = pi * radius**2
+        side_tank = 2 * (pi * (radius*height)) * 2
+        area_tank = top_tank + side_tank
+        sqft = area_tank / 144
+        cost_material = 25 * sqft
+        cost_labor = 15 * sqft
+        estimate_total = "${:,.2f}".format(round(cost_labor + cost_material, 2))
+        return render_template('estimate.html', estimate = estimate_total)
+    return render_template('estimate.html', pageTitle= 'Vertical Tank Maintenance Estimator')
 
 @app.route('/index')
 def index():
