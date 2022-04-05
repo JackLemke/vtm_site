935238b14704fea6dc84ef9e20016bf6f8f84d7d
JackLemke <jackson-lemke@uiowa.edu>
2022-04-04 19:32:13 -0500

    updated app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+   
 from flask import Flask
 from flask import render_template
 
@@ -5,11 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle = 'Flask Server Home Page')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/jack')
+def jack():
+    return render_template('jack.html', pageTitle = 'About Jack')
 
 if __name__ == '__main__':
     app.run(debug=True)
